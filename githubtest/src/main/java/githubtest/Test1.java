97a5d08d227c7b1f843bc4b5c5db638a6c7f9796
13554562871 <2209192978@qq.com>
2019-05-07 20:29:51 +0800

    l

--- githubtest/src/main/java/githubtest/Test1.java
+++ githubtest/src/main/java/githubtest/Test1.java
@@ -2,11 +2,8 @@ package githubtest;
 
 public class Test1 {
 	public static void main(String[] args) {
-		//dddddddddddddddd
 		System.out.println();
 		System.out.println(1);
 		System.out.println(4);
-		System.out.println(9);	
-		
 	}
 }
